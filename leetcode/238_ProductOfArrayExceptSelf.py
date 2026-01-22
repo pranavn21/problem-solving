819a6c9dead023a4bb29c03d3bd8160df9415de7
pranavn21 <pxn190027@utdallas.edu>
2026-01-21 23:59:02 -0600

    added comments for 238

--- leetcode/238_ProductOfArrayExceptSelf.py
+++ leetcode/238_ProductOfArrayExceptSelf.py
@@ -1,14 +1,29 @@
+from typing import List
+
+# Leetcode Problem 238: Product of Array Except Self
+# Link: https://leetcode.com/problems/product-of-array-except-self/
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
+        # Initialize result array with 1s
+        # res[i] will store the product of all elements except nums[i]
+        res = [1] * len(nums)
 
-        prefix = 1
+        # First pass: left to right
+        # res[i] stores the product of all elements to the LEFT of index i
+        prefix = 1  # Running product from the left
         for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
+            res[i] = prefix  # Store product of all elements before i
+            prefix *= nums[i]  # Update prefix for next iteration
+
+        # Second pass: right to left
+        # Multiply res[i] by the product of all elements to the RIGHT of index i
+        postfix = 1  # Running product from the right
         for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
+            res[i] *= postfix  # Multiply by product of all elements after i
+            postfix *= nums[i]  # Update postfix for next iteration
+
         return res
-    
+
+# Time complexity: O(n), where n is the number of elements in nums (two passes through the array)
+# Space complexity: O(1), not counting the output array (only using constant extra space for prefix and postfix)
